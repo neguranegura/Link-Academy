8ed08126621d9d31364932261db442f17e3cef23
PHP Grupa 1 <phpgrupa1@Admins-iMac-57.local>
2018-11-12 18:17:02 +0200

    AA

---
+++ core_php/variabile.php
@@ -0,0 +1,10 @@
+<<?php
+  $firstname = 'Ion';
+  $firstnName = 'Vasile';
+  echo "$firstname, $firstname, $FirstName".PHP_EOL;
+
+  define(MYCONSTANT, 'SOMETHING');
+  echo MYCONSTANT.PHP_EOL;
+  echo "$firstname".PHP_EOL.MYCONSTANT.PHP_EOL;
+  echo "MYCONSTANT".PHP_EOL;
+?>
